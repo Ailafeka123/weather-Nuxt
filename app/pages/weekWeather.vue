f4e257484df9a9230032014f51026f0d4f3fbc50
Ailafeka <ailafeka@gmail.com>
2025-08-23 20:01:50 +0800

    fix:修正一周資料顯示
    1.修正weekWeather的資料排序與讀取
    2.新增一周資料的讀取，可以藉由select去選取地區，並重新抓取資料

--- app/pages/weekWeather.vue
+++ app/pages/weekWeather.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
 import {ref,onMounted, watch, } from 'vue';
 import { useRoute } from 'vue-router';
+
+interface weatherDataList {
+    time?:string,
+    maxT?:string,
+    minT?:string,
+    weather?:string,
+}
+
+interface showDataType {
+    name ?: string,
+    weatherData ?: weatherDataList[]
+}
+
 // 捕捉共用參數
 const areaSelect  = useNuxtApp().$areaSelect as Ref <{name:string}>;
 const areaList = useNuxtApp().$areaList as Ref<string[]>;
@@ -26,14 +39,10 @@ const weatherDataSave = ref<any[]>([]);
 // 設定一周時間
 const showDay = ref<string[]>([]);
 // 要展示的資料 一周 分別 name<string>(地區) MaxT<number[7]>(當日最高) minT<number[7]>(當日最低)  weather<number[7]>(天氣找最大數值)
-const showWeatherData = ref<any[]>([]);
-// 今天
-// const nowDate = ref<Date>(new Date() );
+const showWeatherData = ref<showDataType[]>([]);
 
 // 抓取資料
 const loading7DayWeather = async() =>{
-    console.log("抓取資料");
-    console.log(selectArea.value);
     try{
         apiLoading.value = true;
         const weatherData = await $fetch<{success:boolean, data:any}>('/api/detailWeather',{
@@ -42,16 +51,18 @@ const loading7DayWeather = async() =>{
         });
         // 更新資料到本地保存
         weatherDataSave.value = weatherData.data.records.Locations[0].Location;
+        const weekDay : string[] = [];
         // 捕捉所有時間點
         let lastDay:string = "";
         weatherData.data.records.Locations[0].Location[0].WeatherElement[0].Time.forEach((index:any)=>{
             const day:Date = new Date(index.StartTime);
             const dayString:string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`
             if(lastDay !== dayString){
-                showDay.value.push(dayString);
+                weekDay.push(dayString);
                 lastDay = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`;
             }
         })
+        showDay.value = weekDay;
     }catch(e){
         console.error("錯誤:",e);
     }finally{
@@ -71,92 +82,151 @@ onMounted(async()=>{
         await loading7DayWeather();
     }
 })
-// 本地同步共用
+// 本地同步到plugins共用
 watch(selectArea,()=>{
     areaSelect.value.name = selectArea.value
 })
 
 // 當資料更新的時候，更新展示資料
 watch(weatherDataSave,()=>{
+    if (!weatherDataSave) return ;
+    const tempData : showDataType[] = [];
     weatherDataSave.value.forEach((index:any)=>{
-        const temp :any = [index.LocationName];
-        const dayList :any[] = Array(showDay.value.length);
-        for(let i = 0 ; i < dayList.length ; i++){
-            dayList[i] = [];
-        }
-        // 找到 最高溫度 最低溫度 天氣現象 的數字 然後丟到dayList裡面
-        for(let i = 0 ; i < index.WeatherElement.length; i++){
-            let lastDay:string = "";
-            let times : number = 0;
-            if(index.WeatherElement[i].ElementName === "最高溫度" ){
-                let maxT:number = -Infinity;
-                index.WeatherElement[i].Time.forEach((index:any)=>{
-                    let day : Date = new Date(index.StartTime); 
-                    const dayString : string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`
-                    if(dayString !== lastDay){
-                        if(lastDay !== ""){
-                            dayList[times].maxT = maxT;
-                            times++;
+        const temp : showDataType = {name:index.LocationName};
+        const tempMap = new Map<string, weatherDataList>();
+        index.WeatherElement.forEach((itemIndex:any)=>{
+            if(itemIndex.ElementName === "最高溫度"){
+                let lastDay : string = "";
+                let MaxT : number = -Infinity
+                itemIndex.Time.forEach((timeIndex:any)=>{
+                    const day:Date = new Date(timeIndex.StartTime);
+                    const dayString:string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`;
+                    if(lastDay === dayString){
+                        MaxT = Math.max(MaxT , timeIndex.ElementValue[0].MaxTemperature);
+                    }else if(lastDay === ""){
+                        lastDay = dayString;
+                        MaxT = timeIndex.ElementValue[0].MaxTemperature;
+                    }else{
+                        if(tempMap.has(lastDay)){
+                            let getMapIndex = tempMap.get(lastDay);
+                            if(getMapIndex){
+                                getMapIndex.maxT = String(MaxT);
+                            }else{
+                                getMapIndex = {time:lastDay, maxT:String(MaxT)};
+                            }
+                            tempMap.set(lastDay, getMapIndex);
+                        }else{
+                            tempMap.set(lastDay,{time:lastDay, maxT:String(MaxT)})
                         }
                         lastDay = dayString;
-                        maxT = parseInt(index.ElementValue[0].MaxTemperature);
-                    }else{
-                        maxT = Math.max(maxT, parseInt(index.ElementValue[0].MaxTemperature) );
+                        MaxT = timeIndex.ElementValue[0].MaxTemperature;
                     }
                 })
-                dayList[times].maxT =maxT;
-            }else if(index.WeatherElement[i].ElementName === "最低溫度"){
-                let minT:number = Infinity;
-                index.WeatherElement[i].Time.forEach((index:any)=>{
-                    let day : Date = new Date(index.StartTime); 
-                    const dayString : string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`
-                    if(dayString !== lastDay){
-                        if(lastDay !== ""){
-                            dayList[times].minT = minT;
-                            times++;
+                if(tempMap.has(lastDay)){
+                    let getMapIndex = tempMap.get(lastDay);
+                    if(getMapIndex){
+                        getMapIndex.maxT = String(MaxT);
+                    }else{
+                        getMapIndex = {time:lastDay, maxT:String(MaxT)};
+                    }
+                    tempMap.set(lastDay, getMapIndex);
+                }else{
+                    tempMap.set(lastDay,{time:lastDay, maxT:String(MaxT)})
+                }
+            }else if(itemIndex.ElementName === "最低溫度"){
+                let lastDay : string = "";
+                let MinT : number = Infinity;
+                itemIndex.Time.forEach((timeIndex:any)=>{
+                    const day:Date = new Date(timeIndex.StartTime);
+                    const dayString:string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`;
+                    if(lastDay === dayString){
+                        MinT = Math.min(MinT , timeIndex.ElementValue[0].MinTemperature);
+                    }else if(lastDay === ""){
+                        lastDay = dayString;
+                        MinT = timeIndex.ElementValue[0].MinTemperature;
+                    }else{
+                        if(tempMap.has(lastDay)){
+                            let getMapIndex = tempMap.get(lastDay);
+                            if(getMapIndex){
+                                getMapIndex.minT = String(MinT);
+                            }else{
+                                getMapIndex = {time:lastDay, maxT:String(MinT)};
+                            }
+                            tempMap.set(lastDay, getMapIndex);
+                        }else{
+                            tempMap.set(lastDay,{time:lastDay, maxT:String(MinT)})
                         }
                         lastDay = dayString;
-                        minT = Math.min(minT, parseInt(index.ElementValue[0].MinTemperature))
-                    }else{
-                        minT = parseInt(index.ElementValue[0].MinTemperature);
+                        MinT = timeIndex.ElementValue[0].MinTemperature;
                     }
                 })
-                dayList[times].minT = minT;
-            }else if(index.WeatherElement[i].ElementName === "天氣現象"){
-                let weatherNumber : number = 0;
+                if(tempMap.has(lastDay)){
+                    let getMapIndex = tempMap.get(lastDay);
+                    if(getMapIndex){
+                        getMapIndex.minT = String(MinT);
+                    }else{
+                        getMapIndex = {time:lastDay, maxT:String(MinT)};
+                    }
+                    tempMap.set(lastDay, getMapIndex);
+                }else{
+                    tempMap.set(lastDay,{time:lastDay, maxT:String(MinT)})
+                }
+            }else if(itemIndex.ElementName === "天氣現象"){
+                let lastDay : string = "";
                 let weatherString : string = "";
-                index.WeatherElement[i].Time.forEach((index:any)=>{
-                    let day : Date = new Date(index.StartTime); 
-                    const dayString : string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`
-                    if (dayString !== lastDay){
-                        if(lastDay !== ""){
-                            dayList[times].weather = weatherString;
-                            times++;
+                let weatherNumber : number = 0;
+                itemIndex.Time.forEach((timeIndex:any)=>{
+                    const day:Date = new Date(timeIndex.StartTime);
+                    const dayString:string = `${ String( day.getMonth()+1 ).padStart(2, '0') }/${ String( day.getDate() ).padStart(2, '0') }`;
+                    if(lastDay === dayString){
+                        if(parseInt(timeIndex.ElementValue[0].WeatherCode) > weatherNumber){
+                            weatherString = timeIndex.ElementValue[0].Weather;
                         }
+                    }else if(lastDay === ""){
+                        weatherNumber = parseInt(timeIndex.ElementValue[0].WeatherCode);
+                        weatherString = timeIndex.ElementValue[0].Weather;
                         lastDay = dayString;
-                        weatherNumber = parseInt(index.ElementValue[0].WeatherCode);
-                        weatherString = index.ElementValue[0].Weather;
                     }else{
-                        if(weatherNumber > parseInt(index.ElementValue[0].WeatherCode) ){
-                            weatherString = index.ElementValue[0].Weather;
+                        if(tempMap.has(lastDay)){
+                            let getMapList = tempMap.get(lastDay);
+                            if(getMapList){
+                                getMapList.weather = weatherString;
+                            }else{
+                                getMapList = {time:lastDay, weather : weatherString};
+                            }
+                            tempMap.set(lastDay, getMapList);
+                        }else{
+                            tempMap.set(lastDay, {time:lastDay, weather: weatherString});
                         }
+                        weatherNumber = parseInt(timeIndex.ElementValue[0].WeatherCode);
+                        weatherString = timeIndex.ElementValue[0].Weather;
+                        lastDay = dayString;
                     }
                 })
-                dayList[times].weather = weatherString;
-            }else{
-                continue;
+                if(tempMap.has(lastDay)){
+                    let getMapList = tempMap.get(lastDay);
+                    if(getMapList){
+                        getMapList.weather = weatherString;
+                    }else{
+                        getMapList = {time:lastDay, weather : weatherString};
+                    }
+                    tempMap.set(lastDay, getMapList);
+                }else{
+                    tempMap.set(lastDay, {time:lastDay, weather: weatherString});
+                }
             }
-        }
-        temp.push([...dayList]);
-        showWeatherData.value.push(temp);
+        })
+        const sortMapList:weatherDataList[] = Array.from( tempMap.values() ).sort( (a, b) =>
+        (a.time??"").localeCompare(b.time??"") );
+        temp.weatherData = sortMapList;
+        tempData. push(temp);
     })
+    showWeatherData.value = tempData;
 })
 
 watch(loading, async()=>{
     if(loading.value === false) return ;
     await loading7DayWeather();
-    // console.log(weatherDataSave.value)
-    // console.log(showDay.value)
     loading.value = false;
 })
 
@@ -164,6 +234,17 @@ watch(loading, async()=>{
 </script>
 
 <style module="style" lang="scss">
+    .articelHeader{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding:24px 0;
+        gap: 16px;
+        h3{
+            margin:0;
+        }
+    }
     .cardDivContent{
         display: flex;
         flex-direction: column;
@@ -213,40 +294,46 @@ watch(loading, async()=>{
 
 <template>
     <article>
-        <h3>目前查詢 {{selectArea}} 一周天氣預報</h3>
-        <!-- <select v-model="selectArea" @change="loading = true">
-            <option v-for="area in areaList" :key="area" :value="area">
-                {{ area }}
-            </option>
-        </select> -->
-        <div v-if="apiLoading">
-            <h2>is loading</h2>
-        </div>
-
-        <div v-else :class="style.cardDivContent">
-
-            <div :class="style.cardTitle">
-                <div :class="style.cardTitleAreaName ,style.cardItem">
-                    <p>地區</p>
-                </div>
-                <div v-for="(day ,dayKey) in showDay" :key="dayKey" :class="style.cardTitleTime ,style.cardItem">
-                    <p>{{ day }}</p>
-                </div>
+        <header :class="style.articelHeader">
+            <h3>目前查詢: {{selectArea}} 一周天氣預報</h3>
+            <client-only>
+                <select v-model="selectArea" @change="loading = true">
+                    <option v-for="area in areaList" :key="area" :value="area">
+                        {{ area }}
+                    </option>
+                </select>
+            </client-only>
+        </header>
+        <section>
+            <div v-if="apiLoading">
+                <h2>is loading</h2>
             </div>
-
-            <div v-for="(row, rowKey) in showWeatherData" :key="rowKey" :class="style.cardDiv">
-                <div :class="style.cardItem">
-                    {{ row[0] }}
-                </div>
-                <div v-for="(rowData, DataKey) in row[1]" :key="`${DataKey}-${rowKey}`" :class="style.cardItem">
-                    <div >
-                        <p>{{ rowData.weather}}</p>
+            
+            <div v-else :class="style.cardDivContent">
+                
+                <div :class="style.cardTitle">
+                    <div :class="style.cardTitleAreaName ,style.cardItem">
+                        <p>地區</p>
                     </div>
-                    <div>
-                        <p>{{ rowData.minT }}℃ ~ {{ rowData.maxT }}℃</p>
+                    <div v-for="(day ,dayKey) in showDay" :key="dayKey" :class="style.cardTitleTime ,style.cardItem">
+                        <p>{{ day }}</p>
                     </div>
                 </div>
+                
+                <div v-for="(row, rowKey) in showWeatherData" :key="`area= ${row.name}`" :class="style.cardDiv">
+                    <div :class="style.cardItem">
+                        {{ row.name }}
+                    </div>
+                    <div v-for="(rowData, DataKey) in row.weatherData" :key="`${row.name}-${rowKey}`" :class="style.cardItem">
+                        <div >
+                            <p>{{ rowData.weather}}</p>
+                        </div>
+                        <div>
+                            <p>{{ rowData.minT }}℃ ~ {{ rowData.maxT }}℃</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     </article>
 </template>
